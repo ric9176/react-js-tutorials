38435b124fdfa2d2dff7e22d2029aa5bb365a8f1
Richard Moss <richard@englishtutorslive.com>
2016-10-23 14:02:30 +0100

    basic redux implementation in console

--- 1-basic-react/src/js/client.js
+++ 1-basic-react/src/js/client.js
@@ -1,7 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { createStore } from "redux";
 
-import Layout from "./components/Layout";
+const reducer = function (state, action) {
+  if (action.type === "INC") {
+    return state+action.payload;
+  }
+  if (action.type === "DEC") {
+    return state-action.payload;
+  }
+  return state;
+};
 
-const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app);
+const store = createStore(reducer, 0);
+
+store.subscribe(() => {
+  console.log("store changed!", store.getState());
+});
+
+store.dispatch({type: "INC", payload: 1});
+store.dispatch({type: "INC", payload: 1});
+store.dispatch({type: "INC", payload: 5});
+store.dispatch({type: "INC", payload: 1});
+store.dispatch({type: "INC", payload: 1});
+store.dispatch({type: "DEC", payload: 1});
+// import React from "react";
+// import ReactDOM from "react-dom";
+//
+// import Layout from "./components/Layout";
+//
+// const app = document.getElementById('app');
+// ReactDOM.render(<Layout/>, app);
